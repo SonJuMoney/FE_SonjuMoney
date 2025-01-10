06d7c4098f7a324cd9cb31357dc06f187bb0eff1
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2025-01-10 23:09:43 +0900

    [#2] feat : Photo Input 추가

--- components/atoms/Cards/RegisterCard.tsx
+++ components/atoms/Cards/RegisterCard.tsx
@@ -1,11 +1,11 @@
 import { RegisterCardProps } from '@/types/register';
+import { LuPlus } from 'react-icons/lu';
 
-// + 아이콘 추가
 const RegisterCard = ({ text }: RegisterCardProps) => {
   return (
     <div className='flex flex-col items-center justify-center bg-white rounded-xl p-4 border border-[#eaecef]'>
       <div className='flex items-center justify-center w-10 h-10 rounded-full bg-appColor text-white'>
-        +
+        <LuPlus className='w-[18px] h-[18px]' />
       </div>
 
       <p className='mt-2 text-appColor font-bold text-[15px]'>{text}</p>
