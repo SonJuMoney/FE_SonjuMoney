d9210b8d3a3d657ad726977874fe554777adfbcf
ezenjun <ezenjun07@gmail.com>
2025-01-10 13:28:02 +0900

    [#1] feat : 색상 정의 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ['var(--font-pretendard)'],
+      },
       colors: {
-        background: 'var(--background)',
+        appColor: '#FF9100',
+        secondary: '#F8B62D',
+        black: '#212529',
+        placeHolder: '#B9B9B9',
+        disabled: '#DEDEDE',
+        white: '#FFFFFF',
+        darkGray: '#555555',
+        error: '#EA4335',
+        lavendar: '#D3B1FF',
+        lemon: '#FCF094',
+        skyBlue: '#BEE1F0',
+        peach: '#F5C2C2',
+        brown: '#C6A284',
+        pink: '#FF8086',
+        background: '#F3F4F6',
         foreground: 'var(--foreground)',
       },
     },
