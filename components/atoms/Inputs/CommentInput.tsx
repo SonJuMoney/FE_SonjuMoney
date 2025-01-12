6c30390b8de5413860ef8df91845a7610e970888
ezenjun <ezenjun07@gmail.com>
2025-01-12 22:59:07 +0900

    [#2] feat : CommentInput 생성

---
+++ components/atoms/Inputs/CommentInput.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import CommentOff from '@/assets/Icons/commentOff_32.svg';
+import CommentOn from '@/assets/Icons/commentOn_32.svg';
+import { useCallback, useRef, useState } from 'react';
+
+interface CommentInputProps {
+  onSubmit?: (comment: string) => void;
+}
+
+export default function CommentInput({ onSubmit }: CommentInputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [hasValue, setHasValue] = useState(false);
+
+  const handleInput = useCallback(() => {
+    setHasValue(!!inputRef.current?.value.trim());
+  }, []);
+
+  const handleSubmit = useCallback(() => {
+    const input = inputRef.current;
+    if (input && input.value.trim()) {
+      onSubmit?.(input.value);
+      input.value = '';
+      setHasValue(false);
+    }
+  }, [onSubmit]);
+  console.log('hi');
+
+  return (
+    <div className='fixed bottom-0 left-0 right-0 bg-white px-5 py-4  border-t border-gray-100'>
+      <div className='flex items-center gap-[25px]'>
+        <input
+          ref={inputRef}
+          onInput={handleInput}
+          type='text'
+          placeholder='댓글을 남겨주세요'
+          className='w-full text-black placeholder-placeHolder focus:outline-none'
+        />
+        <button onClick={handleSubmit} className='focus:outline-none'>
+          {hasValue ? (
+            <CommentOn className='w-8 h-8 transition-all' />
+          ) : (
+            <CommentOff className='w-8 h-8 transition-all' />
+          )}
+        </button>
+      </div>
+    </div>
+  );
+}
