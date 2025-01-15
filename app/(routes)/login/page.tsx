2d7c7fa1499b088cdb5837f180b0db81afba08e1
jojoo <csh990720@gmail.com>
2025-01-15 20:49:28 +0900

    [#17] feat: 로그인 기능 구현 완료/ 로그인 관련 라우터 설정

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -1,4 +1,3 @@
-import Header from '@/components/atoms/Headers/Header';
 import PageTitle from '@/components/atoms/PageTitles/PageTitle';
 import { LoginForm } from '@/components/molecules/Forms/LoginForm';
 import Link from 'next/link';
@@ -6,8 +5,7 @@ import Link from 'next/link';
 const Login = () => {
   return (
     <div className='h-screen'>
-      <Header title='로그인' showBackButton={false} />
-      <div className='p-8 flex flex-col gap-8'>
+      <div className='px-8 pt-[76px] flex flex-col gap-8'>
         <div>
           <PageTitle title='손주머니에' />
           <PageTitle title='오신 것을 환영합니다' />
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,28 +1,91 @@
 import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
+declare module 'next-auth' {
+  interface Session {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+
+  interface User {
+    id: string;
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   session: { strategy: 'jwt' },
-  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        email: {
-          label: 'Email',
-          type: 'text',
-          placeholder: 'jsmith@example.com',
-        },
+        userId: { label: 'id', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      async authorize() {
-        return null;
+      async authorize(credentials) {
+        if (!credentials?.userId || !credentials?.password) {
+          return null;
+        }
+
+        try {
+          const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/auth/sign-in`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                authId: credentials.userId,
+                password: credentials.password,
+              }),
+            }
+          );
+
+          if (!response.ok) {
+            console.log('응답이상');
+          }
+
+          const data = await response.json();
+
+          if (response.ok && data.access_token) {
+            // 반드시 user 객체를 반환해야 합니다
+            const user = {
+              id: credentials.userId,
+              accessToken: data.access_token,
+              refreshToken: data.refresh_token,
+            };
+            return user;
+          }
+
+          throw new Error('로그인 api 에러');
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
       },
     }),
   ],
-  callbacks: {},
-
-  pages: {
-    signIn: '/login',
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.accessToken = user.accessToken;
+        token.refreshToken = user.refreshToken;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      session.refreshToken = token.refreshToken;
+      return session;
+    },
   },
 };
