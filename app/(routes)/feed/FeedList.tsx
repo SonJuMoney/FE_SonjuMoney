4018fbbc15fccca6ba92360d2369f1d5b0827915
ezenjun <ezenjun07@gmail.com>
2025-01-19 09:20:17 +0900

    [#41] feat : 피드 게시글 추가 버튼 생성

--- app/(routes)/feed/FeedList.tsx
+++ app/(routes)/feed/FeedList.tsx
@@ -7,7 +7,9 @@ import { useFeedApi } from '@/hooks/useFeedApi/useFeedApi';
 import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
 import { TFeed } from '@/types/Feed';
 import { useInfiniteQuery } from '@tanstack/react-query';
-import { useRef } from 'react';
+import { LuPlus } from 'react-icons/lu';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 
 interface FeedListProps {
   initialData: ResponseType<GetPaginationResult<TFeed>>;
@@ -42,13 +44,48 @@ export default function FeedList({ initialData, familyId }: FeedListProps) {
     enabled: hasNextPage,
   });
 
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      setIsScrolling(true);
+
+      // Reset the scrolling state after scrolling stops
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false);
+      }, 150); // Adjust timeout as needed
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 '>
       {data?.pages.map((page) =>
         page.result?.content.map((feed) => (
           <FeedCard key={feed.feed_id} feed={feed} />
         ))
       )}
+      <Link href='/feed/create' className='fixed bottom-[90px] right-5 z-50'>
+        <div className='flex items-center justify-center gap-1 px-3 py-3 rounded-full bg-appColor text-white'>
+          <LuPlus className='w-[24px] h-[24px]' />
+          <span
+            className={`transition-all duration-200 font-semibold mr-2${
+              isScrolling ? 'w-0 opacity-0 hidden' : 'w-auto opacity-100'
+            }`}
+          >
+            글쓰기
+          </span>
+        </div>
+      </Link>
 
       <div ref={bottomRef} className='w-full pb-5'>
         {isFetchingNextPage && (
