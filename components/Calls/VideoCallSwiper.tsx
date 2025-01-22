24a1f769ba6f36f0a8bfa4adfa36d760ff8882ae
ezenjun <ezenjun07@gmail.com>
2025-01-22 19:41:30 +0900

    [#45] feat : 가족 멤버 api 연동

--- components/Calls/VideoCallSwiper.tsx
+++ components/Calls/VideoCallSwiper.tsx
@@ -2,15 +2,15 @@
 
 // Import Swiper styles
 import CreateMeeting from '@/app/(routes)/call/CreateMeeting';
+import { useFamilyApi } from '@/hooks/useFamilyApi/useFamilyApi';
 import { useSelectedFamilyStore } from '@/store/useSelectedFamilyStore';
+import { TMember } from '@/types/Family';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import { EffectCards } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
-import { ButtonMedium } from '../atoms/Buttons/ButtonMedium';
-import { ButtonSmall } from '../atoms/Buttons/ButtonSmall';
+import { useEffect, useRef, useState } from 'react';
 
 interface User {
   id: string;
@@ -19,24 +19,24 @@ interface User {
   status: 'online' | 'offline';
 }
 
-const mockUsers: User[] = [
-  { id: '2', name: '김민수', avatar: '/avatars/user1.jpg', status: 'online' },
-  { id: '3', name: '이지은', avatar: '/avatars/user2.jpg', status: 'offline' },
-  { id: '4', name: '박준호', avatar: '/avatars/user3.jpg', status: 'online' },
-  { id: '5', name: '박준호', avatar: '/avatars/user3.jpg', status: 'online' },
-  { id: '6', name: '박준호', avatar: '/avatars/user3.jpg', status: 'online' },
-];
-
 export default function VideoCallSwiper() {
-  const { selectedFamily, setSelectedFamily } = useSelectedFamilyStore();
+  const { selectedFamily } = useSelectedFamilyStore();
+  const { getFamilyMembers } = useFamilyApi();
+  const [familyMembers, setFamilyMembers] = useState<TMember[]>([]);
+  useEffect(() => {
+    const fetchMembers = async () => {
+      if (selectedFamily) {
+        const response = await getFamilyMembers(
+          selectedFamily?.family_id,
+          'EXCEPTME'
+        );
+        setFamilyMembers(response.members);
+      }
+    };
 
-  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    fetchMembers();
+  }, []);
   const swiperRef = useRef(null);
-
-  const handleCallUser = (user: User) => {
-    setSelectedUser(user);
-    // Implement your video call logic here
-  };
 
   return (
     <div className='flex flex-col items-center w-ful h-full p-4 mt-20'>
@@ -53,27 +53,22 @@ export default function VideoCallSwiper() {
             slideShadows: false,
           }}
         >
-          {mockUsers.map((user) => (
+          {familyMembers.map((user) => (
             <SwiperSlide
-              key={user.id}
+              key={user.member_id}
               className='bg-white/60 rounded-[15px] border-2 border-white backdrop-blur-xl'
             >
               <div className='flex flex-col items-center justify-between h-full p-6'>
-                <div className='relative w-40 h-40'>
+                <div className='relative w-60 h-60'>
                   <Image
-                    src={'/Role1.png'}
+                    src={user.profile_link || '/Role1.png'}
                     alt={''}
                     fill
                     className='rounded-full object-cover border bg-secondary p-5'
                   />
-                  <div
-                    className={`absolute bottom-4 right-4 w-4 h-4 rounded-full ${
-                      user.status === 'online' ? 'bg-green-500' : 'bg-gray-400'
-                    }`}
-                  />
                 </div>
-                <h3 className='text-xl font-bold mt-4'>{user.name}</h3>
-                <CreateMeeting calleeId={user.id} />
+                <h3 className='text-xl font-bold mt-4'>{user.member_name}</h3>
+                <CreateMeeting calleeId={user.user_id.toString()} />
               </div>
             </SwiperSlide>
           ))}
