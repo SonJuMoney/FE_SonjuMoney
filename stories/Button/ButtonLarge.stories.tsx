4fa169372b7d0360f1e1fda3e07ac94b9b5fdd7f
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2025-01-29 13:44:46 +0900

    [#120] feat : Button, Select storybook

---
+++ stories/Button/ButtonLarge.stories.tsx
@@ -0,0 +1,23 @@
+import { ButtonLarge } from '@/components/atoms/Buttons/ButtonLarge';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof ButtonLarge> = {
+  title: 'Components/Button/ButtonLarge',
+  component: ButtonLarge,
+  argTypes: {
+    disabled: { control: 'boolean' },
+    onClick: { action: 'clicked' },
+  },
+};
+
+export const Large: StoryObj<typeof meta> = {
+  args: {
+    type: 'button',
+    text: '다음',
+    className:
+      'flex justify-center items-center w-full p-4 rounded-[14px] text-lg',
+    disabled: false,
+  },
+};
+
+export default meta;
